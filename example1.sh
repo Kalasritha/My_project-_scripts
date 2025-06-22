4844dde50132808bd6ee43b896b89c0fa2b03705
root <root@Kalasritha.>
2025-06-22 16:05:37 +0000

    name changed with surname

--- example1.sh
+++ example1.sh
@@ -2,5 +2,5 @@
 # this script is for greeting of everryday with my name
 
 
-echo " Good morning,Kalasritha!..Today is $(date)" > /mnt/c/Users/kalas/greeting.txt
+echo " Good morning,Kalasritha Biccavolu!..Today is $(date)" > /mnt/c/Users/kalas/greeting.txt
 
